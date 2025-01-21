0f31e5c1940f66ab477ece09a33701ab7e28894b
Nonprawich I. <40120130+npwitk@users.noreply.github.com>
2025-01-21 14:14:07 +0700

    Add Assignment

---
+++ Assignment 1/EvenOrOdd.swift
@@ -0,0 +1,16 @@
+//
+//  EvenOrOdd.swift
+//  
+//
+//  Created by Nonprawich I. on 21/01/2025.
+//
+
+import Foundation
+
+func isEvenOrOdd(_ n: Int) {
+    if (n % 2 == 0) {
+        print("Even")
+    } else {
+        print("Odd")
+    }
+}
---
+++ Assignment 1/isPrime.swift
@@ -0,0 +1,30 @@
+//
+//  isPrime.swift
+//  
+//
+//  Created by Nonprawich I. on 21/01/2025.
+//
+
+import Foundation
+
+func isPrime(n: Int) -> Bool {
+    if n <= 1 {
+        return false
+    }
+    
+    if n <= 3 {
+        return true
+    }
+    
+    for i in 2...n - 1 {
+        if n % i == 0 {
+            return false
+        }
+    }
+    
+    return true
+
+}
+
+print(isPrime(n: 971)) // return true
+print(isPrime(n: 972)) // return false
